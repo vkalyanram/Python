c54269d4a7f0a04df25839e82205939ee2434d3a
vkalyanram <kalyan.2405@gmail.com>
2022-06-10 18:27:12 +0530

    Read json data

---
+++ test.py
@@ -0,0 +1,10 @@
+import requests
+r = requests.get('https://coderbyte.com/api/challenges/json/age-counting')
+l=list((r.json()['data']).split(','))
+count=0
+for i in range(len(l)):
+  if (i%2==1) and int(l[i].strip()[4:])>=50:
+    count= count+1
+print(count)    
+      
+
