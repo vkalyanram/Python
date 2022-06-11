3354b335411d0db966894b3722a9fc7c006a27ec
vkalyanram <kalyan.2405@gmail.com>
2022-06-11 15:29:20 +0530

    extract name and domain from email

--- test.py
+++ test.py
@@ -1,12 +1,3 @@
-'''import requests
-r = requests.get('https://coderbyte.com/api/challenges/json/age-counting')
-l=list((r.json()['data']).split(','))
-count=0
-for i in range(len(l)):
-  if (i%2==1) and int(l[i].strip()[4:])>=50:
-    count= count+1
-print(count) 
-'''
-   
-      
-
+email="Sailaja@unisys.com"
+n=(email.split('@'))
+print("Hi {0}, Welcome to {1}".format((n[0].capitalize()),(((n[1].split('.'))[0]).capitalize())))
